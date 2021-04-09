5942a60caf92f07251bf7fd984a9677a2402b615
fauzanfebrian <fauzanfebrian1725@gmail.com>
2021-04-09 13:07:33 +0700

    Cors Add

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 const apiContoller = require("../controllers/apiController");
 const { upload_member } = require("../middlewares/multer");
+const cors = require("cors");
 
+router.use(cors());
 router.get("/landing-page", apiContoller.landingPage);
 router.get("/detail-page/:id", apiContoller.detailPage);
 router.post("/booking-page", upload_member, apiContoller.bookingPage);
